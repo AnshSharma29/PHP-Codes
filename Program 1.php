2e167f929f7f8e79bd4865588a9f20af3bdeb4e7
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-05-07 00:20:54 +0530

    Create Program 1.php

---
+++ Program 1.php
@@ -0,0 +1,58 @@
+//WAP to implement regular expressions including modifiers, operators and metacharacters.
+<?php 
+ 
+echo "<h1>Modifiers</h1>";
+echo "<p> i modifier </p>";
+echo "Pattern  = cat <br> String = CAT is not a dog <br>";
+ 
+$pattern1 = "/cat/i";
+ 
+$string ="CAT is not a dog";
+ 
+$result = preg_match($pattern1,$string);
+if($result){
+    echo "<br>cat found";
+}
+else{
+    echo "<br>cat not found";
+}
+ 
+ 
+echo "<h1> Operators</h1> ";
+ 
+echo "Pattern  = ^cat|dog|boy <br> String = CAT is not a dog <br>";
+ 
+$pattern1 = "/^cat|dog|boy/";
+ 
+$string ="CAT is not a dog";
+ 
+$result = preg_match($pattern1,$string);
+if($result){
+    echo "<br>found";
+}
+else{
+    echo "<br>not found";
+}
+ 
+ 
+echo "<h1> Metacharacters </h1> ";
+ 
+echo "Pattern  = ^[a-z\d]+.com <br> String = 123.com is my domain name <br>";
+ 
+$pattern1 = "/[a-z\d]+.com/";
+ 
+$string ="123.com in my domain name";
+ 
+$result = preg_match($pattern1,$string);
+if($result){
+    echo "<br>found";
+}
+else{
+    echo "<br>not found";
+}
+ 
+ 
+ 
+ 
+ 
+?>
