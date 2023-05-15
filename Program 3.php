a8323357616513a1981d0d34e37777abd724aace
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-05-16 00:18:38 +0530

    Create Program 3.php

---
+++ Program 3.php
@@ -0,0 +1,19 @@
+#Write a program to create a menu driven program and to show the usage of switch case.
+
+<?php
+$meal="Breakfast";
+switch($meal){
+    case"Breakfast":
+        echo"It's Breakfast time";
+        break;
+    case"Lunch":
+        echo"It's Lunch time";
+        break;
+    case"Dinner":
+        echo"It's Dinner time";
+        break;
+    default:
+        echo"It's snacks time";
+    }
+ 
+?>
