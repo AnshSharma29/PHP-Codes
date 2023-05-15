7f14666ca1316b66ed5158bc9b9f918e74a64167
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-05-16 00:17:32 +0530

    Update Program 2.php

--- Program 2.php
+++ Program 2.php
@@ -1,15 +1,12 @@
+#WAP to show the usage of nested if statement. 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<?php
+$num=300;
+if($num%2==0)
+{
+    if($num%4==0)
+    {
+        echo"$num is divisible by 2 and 4";
+    }
+}
+?>
