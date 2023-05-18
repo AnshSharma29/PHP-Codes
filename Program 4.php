25bf4d35866c078a7f04aa4a270a34db6294be44
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-05-18 22:54:22 +0530

    Update Program 4.php

--- Program 4.php
+++ Program 4.php
@@ -1,2 +1,27 @@
+//WAP to show the usage of for/while/do while loop.
+
+<?php   
+echo " implementing for loop<br> "; 
+echo"list of numbers are<br> "; 
+$a=10; 
+for($i=0;$i<$a;$i++){ 
+    echo" $i<BR>"; 
+}
+echo "implementing while loop<br> "; 
+echo"list of numbers are<br> "; 
+$a=15; 
+$i=0; 
+while($i<$a){ 
+    echo "$i<br>"; 
+    $i++;
+} 
+echo "implementing  do-while loop<br> "; 
+echo"list of numbers are<br> "; 
+$b=3; 
+do { 
+    echo " $b<br>"; 
+    $b++; 
+} while($b<10); 
+?>
 
 
